fd50eff039353526e25de57e80778606fdb462c0
Manuel Montenegro <mmontene@ucm.es>
2021-03-29 18:50:14 +0200

    Errata en comentario

--- diccionarios/map_tree_v2/main.cpp
+++ diccionarios/map_tree_v2/main.cpp
@@ -7,9 +7,9 @@
  *         Universidad Complutense de Madrid
  * ---------------------------------------------------
  */
- 
+
 /*
- * Uso del TAD Conjunto
+ * Uso del TAD Diccionario
  */
 
 #include "map_tree.h"
@@ -17,31 +17,30 @@
 using namespace std;
 
 struct Coords {
-    double x;
-    double y;
+  double x;
+  double y;
 };
 
-
-
 struct OrdenLexicografico {
-    bool operator()(const Coords &p1, const Coords &p2) const { return p1.x < p2.x || p1.x == p2.x && p1.y < p2.y; }
+  bool operator()(const Coords &p1, const Coords &p2) const {
+    return p1.x < p2.x || p1.x == p2.x && p1.y < p2.y;
+  }
 };
 
 int main() {
-    MapTree<double, string> posiciones;
-    
-    posiciones.insert({-3, "Laura"});
-    posiciones.insert({4.5, "Gloria"});
-    posiciones.insert({2, "Belén"});
-    posiciones.insert({0.5, "Samuel"});
-    
+  MapTree<double, string> posiciones;
 
-    MapTree<Coords, string, OrdenLexicografico> dicc;
-    
-    dicc.insert({{3, 3}, "Laura"});
-    dicc.insert({{5, -3}, "Gloria"});
-    dicc.insert({{1, 2}, "Belén"});
-    dicc.insert({{-1, -5}, "Samuel"});
-    
-    cout << dicc[{3, 3}] << endl;
+  posiciones.insert({-3, "Laura"});
+  posiciones.insert({4.5, "Gloria"});
+  posiciones.insert({2, "Belén"});
+  posiciones.insert({0.5, "Samuel"});
+
+  MapTree<Coords, string, OrdenLexicografico> dicc;
+
+  dicc.insert({{3, 3}, "Laura"});
+  dicc.insert({{5, -3}, "Gloria"});
+  dicc.insert({{1, 2}, "Belén"});
+  dicc.insert({{-1, -5}, "Samuel"});
+
+  cout << dicc[{3, 3}] << endl;
 }
